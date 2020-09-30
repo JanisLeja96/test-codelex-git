0c5edaa0000b6fc8f8e1b0f313607dda48b80dfb
JanisLeja1996 <janis.leja1996@gmail.com>
2020-09-30 10:20:55 +0300

    Added RandomNumber.php

--- RandomNumber.php
+++ RandomNumber.php
@@ -5,7 +5,7 @@ $number = random_int(1, 1000);
 $average = 0;
 if (filesize('randomNumber.txt') > 0) {
     $array = explode(PHP_EOL, fread($file, filesize('randomNumber.txt')));
-    $average = number_format(array_sum($array) / count($array), 2);
+    $average = round(array_sum($array) / count($array), 2);
 }
 
 fwrite($file, $number . PHP_EOL);
